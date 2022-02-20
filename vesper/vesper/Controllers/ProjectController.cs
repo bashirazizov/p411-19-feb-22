21ee2f6ee3760ecd1351e7934ab11ffc87d8c3db
bashirazizov <bashir.azizov@code.edu.az>
2022-02-20 04:38:23 +0400

    init

---
+++ vesper/vesper/Controllers/ProjectController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vesper.DAL;
+
+namespace vesper.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly VesperDbContext db;
+        public ProjectController(VesperDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index(int id)
+        {
+            return View(db.Projects.Include(x=>x.ProjectImages).First(x=>x.Id==id));
+        }
+    }
+}
